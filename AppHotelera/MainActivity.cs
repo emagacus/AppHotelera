7ddfe88882996b7e67ca0e43a0ddf75edbd62bdd
emagacus <emagaca@hotmail.com>
2016-11-25 05:35:12 -0600

    Avance 1

---
+++ AppHotelera/MainActivity.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+
+
+namespace AppHotelera
+{
+    [Activity(Label = "AppHotelera", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Login);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.MyButton);
+
+            button.Click += delegate
+            {
+               
+                StartActivity(typeof(ServicioActivity));
+              
+
+            };
+        }
+
+
+        
+
+
+
+    }
+    
+}
+
---
+++ AppHotelera/ServicioActivity.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace AppHotelera
+{
+    [Activity(Label = "ServicioActivity")]
+    public class ServicioActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Menu);
+            Button b1 = FindViewById<Button>(Resource.Id.button2);
+            b1.Click += delegate { StartActivity(typeof(ValetParking));};
+
+            // Create your application here
+        }
+
+
+     
+
+        public void createDialog()
+        {
+
+
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+            alert.SetTitle("hello");
+            alert.SetMessage("message");
+            alert.SetNegativeButton("Cancel", (senderAlert, args) => {
+                Toast.MakeText(this, "Cancelled!", ToastLength.Short).Show();
+            });
+
+            Dialog dialog = alert.Create();
+            dialog.Show();
+
+
+        }
+
+
+    }
+}
